62aaa3ce3987a53c5f498d48846fc28eb5da1e9e
Iamneo <techadmin@iamneo.ai>
2023-09-07 11:13:13 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';
+  title = 'reg_form';
+  regForm: any = new FormGroup({
+    inputFirstname: new FormControl({ value: '', disabled: false }, [
+      Validators.required, Validators.pattern('^[a-zA-z]'),
+      Validators.minLength(3), Validators.maxLength(50)
+    ]),
+    gender: new FormControl({ value: '', disabled: false }, Validators.required),
+    country: new FormControl({ value: '', disabled: false }, Validators.required),
+    city: new FormControl({ value: '', disabled: false }, Validators.required),
+    inputAge: new FormControl({ value: '', disabled: false }, [Validators.required,Validators.pattern('^[0-9]*$')])
+  });
+  gender = [{ "sex": "Male" }, { "sex": "Female" }];
+  country = [{ "code": "US" }, { "code": "Canada" }, { "code": "India" }, { "code": "Russia" }, { "code": "China" }, { "code": "Dubai" }];
+  submitButton() {
+    console.log(this.regForm)
+  }
+  onCheckChange(eve: any) {
+    if(this.regForm.get('gender').value == ''){
+      this.regForm.get('gender').setValue(eve.target.value)
+    }
+  }
+ selected(eve: any) {
+    if (eve.target.value == 'Canada' || eve.target.value == 'US' ||eve.target.value == 'India') {
+      this.regForm.get('inputAge').setValidators(Validators.required);
+    } else {
+      this.regForm.get('inputAge').clearValidators();
+    }
+    this.regForm.controls['inputAge'].updateValueAndValidity();
+  }
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }
